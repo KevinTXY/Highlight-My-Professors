88d173815847d037b645042aa6a4daeed7fa1b02
Kritika Pradhan <pradhan_kritika406@yahoo.com>
2018-10-13 14:53:15 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,12 @@
+import * as data from 'E:\mhacks 11\result.json'; //loading data from json file
 
+const fullName=data.name; //stores full name
+const rating=data.rating; //stores professor rating
+const takeAgainPerc=data.takeagain; //percentage of students that would take the class again
+const difficultyLevel=data.difficulty;// difficulty level
+const allReviews=data.reviews; //array of all reviews with the most recent being on top
+var length=allReviews.length; //stores number of reviews in the JSON file
+for(var i=0;i<length;i++)
+{
+    
+}
