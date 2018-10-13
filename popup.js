2569e86ad3205c93baefd8d3dc225c4b38745533
Kritika Pradhan <pradhan_kritika406@yahoo.com>
2018-10-13 17:12:53 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,12 +1,22 @@
-import * as data from 'E:\mhacks 11\result.json'; //loading data from json file
-
+//import * as data from './result.json'; //loading data from json file
+var data=JSON.parse('{"name": "Asad  Abukhalil","rating": "3.7","takeagain": "44%","difficulty": "2.2"}')
 const fullName=data.name; //stores full name
 const rating=data.rating; //stores professor rating
 const takeAgainPerc=data.takeagain; //percentage of students that would take the class again
 const difficultyLevel=data.difficulty;// difficulty level
-const allReviews=data.reviews; //array of all reviews with the most recent being on top
-var length=allReviews.length; //stores number of reviews in the JSON file
-for(var i=0;i<length;i++)
-{
+//const allReviews=data.reviews; //array of all reviews with the most recent being on top
+//var length=allReviews.length; //stores number of reviews in the JSON file
+// for(var i=0;i<length;i++)
+// {
     
-}
+// }
+//console.log(fullName);
+document.write("Name:",fullName);
+document.write("<br>");
+document.write("Rating: ",rating,"/5");
+document.write("<br>");
+document.write("Students retaking: ",takeAgainPerc);
+document.write("<br>");
+document.write("Difficulty level: ",difficultyLevel,"/5");
+document.write("<br>");
+
