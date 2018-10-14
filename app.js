95b8ea32707616d14102d9667ef1aed160e7010b
yatharthrawat <rawat.yatharth@gmail.com>
2018-10-13 23:28:57 -0400

    Finally API works, fetch works and chrome.storage works

--- app.js
+++ app.js
@@ -1,18 +1,43 @@
-var flag=false;
-  chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-      // If the received message has the expected format...
-      console.log("Content Script executed");
-      if (msg.text === 'needDOM') {
-          // Call the specified callback, passing
-          // the web-page's DOM content as argument
-            document.addEventListener('mouseup', highlight, flag);
-      }
-  });
+// @ts-check
 
-  function highlight(){
-    var text=document.getSelection().toString();
-    if(text!=''){
-      console.log(text);
-      flag=false;
-    }
+var flag = false;
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+  // If the received message has the expected format...
+  console.log("Content Script executed");
+  if (msg.text === "needDOM") {
+    // Call the specified callback, passing
+    // the web-page's DOM content as argument
+    document.addEventListener("mouseup", highlight, flag);
   }
+});
+
+async function highlight() {
+  var text = document.getSelection().toString();
+  if (!text) return;
+
+  var re = /[A-Z][a-z]+/g;
+
+  var m;
+  var list = [];
+  while ((m = re.exec(text))) {
+    list.push(m[0]);
+  }
+  chrome.storage.sync.get(
+    {
+      school: ""
+    },
+    async function(items) {
+      const uni = items.school;
+      var key = list[0] + "+" + list[list.length - 1]+'+'+uni; // Add University
+      key = key.toLowerCase();
+      var value = await getAPI(key);
+      console.log(value);
+      flag = false;
+    }
+  );
+}
+
+const getAPI = key =>
+  fetch(`https://primeval-rain-199602.appspot.com/${key}`).then(response =>
+    response.json()
+  );
